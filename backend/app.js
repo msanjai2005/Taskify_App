2313ce61a4cd3d5651c60007b0a242b7f5a12961
msanjai2005 <msanjai1012@gmail.com>
2025-12-05 18:20:27 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -37,6 +37,6 @@ app.use('/api/user',userRouter);
 
 
 
-app.listen(PORT,()=>{
-    console.log('server is running on http://localhost:3000');
-})
+// app.listen(PORT,()=>{
+//     console.log('server is running on http://localhost:3000');
+// })
