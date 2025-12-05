32a5672d0026c638742f9bcad09b46ba5e9a4338
msanjai2005 <msanjai1012@gmail.com>
2025-12-05 23:18:45 +0530

    updated

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
     },
     isVerified:{
         type:Boolean,
-        default:true
+        default:false
     },
     verifyOtp:{
         type:String,
